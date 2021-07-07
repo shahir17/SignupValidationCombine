e104ebce9d4270b3fb776fb3760f5583913e66db
Ahmad Shahir Abdul Satar <abdulsataas2@vcu.edu>
2021-07-07 10:33:06 -0400

    Initial Commit

---
+++ SignUpValidationCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignUpValidationCombine
+//
+//  Created by Ahmad Shahir Abdul Satar on 7/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignUpValidationCombine/SignUpValidationCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpValidationCombineApp.swift
+//  SignUpValidationCombine
+//
+//  Created by Ahmad Shahir Abdul Satar on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SignUpValidationCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
